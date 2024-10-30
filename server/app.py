d17ad864548efbf4ba966102147f702e87d78ec8
Mosigisi <emmanuel.mosigisi@student.moringaschool.com>
2024-10-30 05:53:10 +0300

    inittial

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -11,16 +11,41 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
+    if request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username'),
+            created_at=datetime.utcnow(),
+            updated_at=datetime.utcnow()
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        message.body = data.get('body', message.body)
+        message.updated_at = datetime.utcnow()
+        db.session.commit()
+        return jsonify(message.to_dict()), 200
+
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response(jsonify({'message': 'Message deleted'}), 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
