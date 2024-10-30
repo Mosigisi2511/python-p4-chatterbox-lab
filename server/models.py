79cf8afc16a7f5550474695231bfe0f283ccaaee
Mosigisi <emmanuel.mosigisi@student.moringaschool.com>
2024-10-30 05:50:32 +0300

    changes

--- server/models.py
+++ server/models.py
@@ -8,7 +8,15 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Message {self.id}, {self.username}: {self.body}>'
